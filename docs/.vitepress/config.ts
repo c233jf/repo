bb2f6dead75916924d9cb87957e29925b6d1fb68
cjf <1143141874@qq.com>
2024-03-03 14:17:59 +0800

    chore: use inline unocss config

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -410,7 +410,11 @@ export default defineConfig({
   },
   vite: {
     plugins: [
-      UnoCSS(),
+      UnoCSS({
+        shortcuts: {
+          btn: 'p-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2',
+        },
+      }),
       AutoImport({ dts: 'auto-imports.d.ts', imports: ['vue'] }),
       Components({
         dirs: ['components'],
