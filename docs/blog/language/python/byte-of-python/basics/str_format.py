e5d41c67603d439808ff0e85e8fb3b3b6da6973c
chenjiefeng <chen233jf@outlook.com>
2025-06-05 10:00:34 +0000

    docs(python): add byte-of-python docs

---
+++ docs/blog/language/python/byte-of-python/basics/str_format.py
@@ -0,0 +1,24 @@
+# https://learnku.com/docs/byte-of-python/2018/basics/3341#e739d8
+# age = 20
+# name = 'Swaroop'
+
+# print('{0} was {1} years old when he wrote this book'.format(name, age))
+# print('Why is {0} playing with that python?'.format(name))
+
+# print('{} was {} years old when he wrote this book'.format(name, age))
+# print('Why is {} playing with that python?'.format(name))
+
+# # 取十进制小数点后的精度为 3 ，得到的浮点数为 '0.333'
+# print('{0:.3f}'.format(1.0/3))
+# # 填充下划线 (_) ，文本居中
+# # 将 '___hello___' 的宽度扩充为 11
+# print('{0:_^11}'.format('hello'))
+# # 用基于关键字的方法打印显示 'Swaroop wrote A Byte of Python'
+# print('{name} wrote {book}'.format(name='Swaroop', book='A Byte of Python'))
+
+# print('a', end='')
+# print('b', end='')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
