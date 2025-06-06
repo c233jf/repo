5f0d550051c15eb7531bee61795a8c96cfb58410
chenjiefeng <chen233jf@outlook.com>
2025-06-06 07:34:12 +0000

    docs(python): add python op_exp docs

---
+++ docs/blog/language/python/byte-of-python/op_exp/index.py
@@ -0,0 +1,64 @@
+# print(3 + 5)
+# print('a' + 'b')
+
+# print(50 - 24)
+
+# print(2 * 3)
+# print('la' * 3)
+
+# print(3 ** 4)
+
+# print(13 / 3)
+
+# print(13 // 3)
+# print(-13 // 3)
+# print(9 // 1.81)
+
+# print(13 % 3)
+# print(-25.5 % 2.25)
+
+# print(2 << 2)
+
+# print(11 >> 1)
+
+# print(5 & 3)
+
+# print(5 | 3)
+
+# print(5 ^ 3)
+
+# print(~5)
+
+# print(5 < 3)
+# print(3 < 5)
+# print(3 < 5 < 7)
+
+# print(5 > 3)
+
+# x = 4
+# print(x >= 3)
+
+# x = 2
+# y = 2
+# print(x == y)
+# x = 'str'
+# y = 'stR'
+# print(x == y)
+# x = 'str'
+# y = 'str'
+# print(x == y)
+
+# x = 2
+# y = 3
+# print(x != y)
+
+# x = True
+# print(not x)
+
+# x = False
+# y = True
+# print(x and y)
+
+# x = True
+# y = False
+# print(x or y)
