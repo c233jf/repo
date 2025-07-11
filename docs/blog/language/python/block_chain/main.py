4fd171cbe83cfb186c646764ff4eeb82e774cb82
chenjiefeng <chen233jf@outlook.com>
2025-07-11 09:56:05 +0000

    chore(python): init block chain doc

---
+++ docs/blog/language/python/block_chain/main.py
@@ -0,0 +1,28 @@
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item):
+    return {"item_id": item_id, "item": item}
