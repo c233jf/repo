eedcf2c3e935b1a88aa601f560b4501d87da5f10
chenjiefeng <1143141874@qq.com>
2025-07-29 07:50:33 +0000

    docs(typescript): add type-challenges 645

---
+++ docs/blog/language/typescript/type-challenges/645/index.ts
@@ -0,0 +1,67 @@
+/*
+  645 - Diff
+  -------
+  by ZYSzys (@ZYSzys) #中等 #object
+
+  ### 题目
+
+  获取两个接口类型中的差值属性。
+
+  ```ts
+  type Foo = {
+    a: string;
+    b: number;
+  }
+  type Bar = {
+    a: string;
+    c: boolean
+  }
+
+  type Result1 = Diff<Foo,Bar> // { b: number, c: boolean }
+  type Result2 = Diff<Bar,Foo> // { b: number, c: boolean }
+
+  ```
+
+  > 在 Github 上查看：https://tsch.js.org/645/zh-CN
+*/
+
+/* _____________ 你的代码 _____________ */
+
+type Diff<O, O1> = {
+  [K in keyof O | keyof O1 as K extends keyof O
+    ? K extends keyof O1
+      ? never
+      : K
+    : K]: K extends keyof O ? O[K] : K extends keyof O1 ? O1[K] : never
+}
+
+/* _____________ 测试用例 _____________ */
+import type { Equal, Expect } from '@type-challenges/utils'
+
+type Foo = {
+  name: string
+  age: string
+}
+type Bar = {
+  name: string
+  age: string
+  gender: number
+}
+type Coo = {
+  name: string
+  gender: number
+}
+
+export type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>,
+]
+
+/* _____________ 下一步 _____________ */
+/*
+  > 分享你的解答：https://tsch.js.org/645/answer/zh-CN
+  > 查看解答：https://tsch.js.org/645/solutions
+  > 更多题目：https://tsch.js.org/zh-CN
+*/
