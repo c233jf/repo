48944245a8c6d438148eea9a7016bebda9068893
chenjiefeng <1143141874@qq.com>
2025-08-12 08:32:43 +0000

    docs(python): add heapq_heap_sorting_algorithm

---
+++ docs/blog/language/python/python_module_of_the_week/data_structures/heapq_heap_sorting_algorithm/heapq_showtree.py
@@ -0,0 +1,22 @@
+from io import StringIO
+import math
+
+
+def show_tree(tree: list[int], total_width: int = 36, fill: str = ' '):
+    """漂亮的打印一棵树。"""
+    output = StringIO()
+    last_row = -1
+    for i, n in enumerate(tree):
+        if i:
+            row = int(math.floor(math.log(i + 1, 2)))
+        else:
+            row = 0
+        if row != last_row:
+            output.write('\n')
+        columns = 2 ** row
+        col_width = int(math.floor(total_width / columns))
+        output.write(str(n).center(col_width, fill))
+        last_row = row
+    print(output.getvalue())
+    print('-' * total_width)
+    print()
