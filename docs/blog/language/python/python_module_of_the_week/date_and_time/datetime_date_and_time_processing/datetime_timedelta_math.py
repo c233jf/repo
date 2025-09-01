1411f1e98acab0b11122b1a1dcfaf05b7f0fb79e
chenjiefeng <1143141874@qq.com>
2025-09-01 08:17:02 +0000

    docs(python): add datetime_date_and_time_processing

---
+++ docs/blog/language/python/python_module_of_the_week/date_and_time/datetime_date_and_time_processing/datetime_timedelta_math.py
@@ -0,0 +1,13 @@
+import datetime
+
+
+one_day = datetime.timedelta(days=1)
+print('1 day    :', one_day)
+print('5 days   :', one_day * 5)
+print('1.5 days :', one_day * 1.5)
+print('1/4 day  :', one_day / 4)
+
+# 假设午饭时间为 1 小时
+work_day = datetime.timedelta(hours=7)
+meeting_length = datetime.timedelta(hours=1)
+print('meetings per day :', work_day / meeting_length)
