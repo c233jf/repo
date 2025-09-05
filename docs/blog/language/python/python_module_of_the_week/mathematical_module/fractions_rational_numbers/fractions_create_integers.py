09bad2f551b9d48f371d83c3e8c15fc642e20a91
chenjiefeng <1143141874@qq.com>
2025-09-05 06:36:20 +0000

    docs(python): add fractions_rational_numbers

---
+++ docs/blog/language/python/python_module_of_the_week/mathematical_module/fractions_rational_numbers/fractions_create_integers.py
@@ -0,0 +1,6 @@
+import fractions
+
+
+for n, d in [(1, 2), (2, 4), (3, 6)]:
+    f = fractions.Fraction(n, d)
+    print('{}/{} = {}'.format(n, d, f))
---
+++ docs/blog/language/python/python_module_of_the_week/mathematical_module/fractions_rational_numbers/fractions_create_strings.py
@@ -0,0 +1,6 @@
+import fractions
+
+
+for s in ['1/2', '2/4', '3/6']:
+    f = fractions.Fraction(s)
+    print('{} = {}'.format(s, f))
