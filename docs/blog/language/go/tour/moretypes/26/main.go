21bfc9672ad2a0f6242ae2427abcf2feaabdb7fc
chenjiefeng <1143141874@qq.com>
2025-09-08 06:10:32 +0000

    docs(go): add go-tour moretypes 26

---
+++ docs/blog/language/go/tour/moretypes/26/main.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func fibonacci() func() int {
+	a, b := 0, 1
+	return func() int {
+		a, b = b, a+b
+		return a
+	}
+}
+
+func main() {
+	f := fibonacci()
+	for range 10 {
+		fmt.Println(f())
+	}
+}
