226eb62d43b008aa6c71c753bedd68aca996db60
chenjiefeng <1143141874@qq.com>
2025-09-09 10:10:02 +0000

    docs(python): add math_mathematical_functions

---
+++ docs/blog/language/python/python_module_of_the_week/mathematical_module/math_mathematical_functions/math_modf.py
@@ -0,0 +1,5 @@
+import math
+
+
+for i in range(6):
+    print(f'{i}/2 = {math.modf(i / 2.0)}')
---
+++ docs/blog/language/python/python_module_of_the_week/mathematical_module/math_mathematical_functions/math_sqrt.py
@@ -0,0 +1,9 @@
+import math
+
+
+print(math.sqrt(9.0))
+print(math.sqrt(3))
+try:
+    print(math.sqrt(-1))
+except ValueError as err:
+    print('Cannot compute sqrt(-1):', err)
