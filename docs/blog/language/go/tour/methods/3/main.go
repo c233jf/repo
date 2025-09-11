ca1af9c749c293009eaef3162076b2036aedd6af
chenjiefeng <1143141874@qq.com>
2025-09-11 03:16:03 +0000

    docs(go): add go-tour methods 3

---
+++ docs/blog/language/go/tour/methods/3/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type MyFloat float64
+
+func (f MyFloat) Abs() float64 {
+	return math.Abs(float64(f))
+}
+
+func main() {
+	f := MyFloat(-math.Sqrt2)
+	fmt.Println(f.Abs())
+}
