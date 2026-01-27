451f3c2c41a5a307b18e69d192ac578d128e8ec4
chenjiefeng <1143141874@qq.com>
2026-01-27 02:53:49 +0000

    fix(docs): 修复无法显示 404 页面

--- scripts/ecs-task.py
+++ scripts/ecs-task.py
@@ -9,6 +9,7 @@ import base64
 import time
 import logging
 
+
 # Configure the log output formatter
 logging.basicConfig(level=logging.INFO,
                     format="%(asctime)s %(name)s [%(levelname)s]: %(message)s",
@@ -16,15 +17,15 @@ logging.basicConfig(level=logging.INFO,
 
 logger = logging.getLogger()
 
-access_key = sys.argv[1] # 设置您的AccessKey ID
-access_key_secret = sys.argv[2] # 设置您的AccessKey Secret
-region_id = sys.argv[3] # 实例所属地域ID
-ins_id = sys.argv[4] # 实例ID
-vpc_registry = sys.argv[5] # 镜像专用网络地址
-
+access_key = sys.argv[1]  # 设置您的AccessKey ID
+access_key_secret = sys.argv[2]  # 设置您的AccessKey Secret
+region_id = sys.argv[3]  # 实例所属地域ID
+ins_id = sys.argv[4]  # 实例ID
+vpc_registry = sys.argv[5]  # 镜像专用网络地址
 
 
 client = AcsClient(access_key, access_key_secret, region_id)
+
 
 def base64_decode(content, code='utf-8'):
     if sys.version_info.major == 2:
@@ -39,13 +40,14 @@ def get_invoke_result(invoke_id):
 
     request.set_InvokeId(invoke_id)
     response = client.do_action_with_exception(request)
-    response_detail = json.loads(response)["Invocation"]["InvocationResults"]["InvocationResult"][0]
-    status = response_detail.get("InvocationStatus","")
-    output = base64_decode(response_detail.get("Output",""))
-    return status,output
+    response_detail = json.loads(
+        response)["Invocation"]["InvocationResults"]["InvocationResult"][0]
+    status = response_detail.get("InvocationStatus", "")
+    output = base64_decode(response_detail.get("Output", ""))
+    return status, output
 
 
-def run_command(cmdtype,cmdcontent,timeout=60):
+def run_command(cmdtype, cmdcontent, timeout=60):
     """
     cmdtype: 命令类型： RunBatScript;RunPowerShellScript;RunShellScript
     cmdcontent: 命令内容
@@ -67,16 +69,17 @@ def run_command(cmdtype,cmdcontent,timeout=60):
         logger.error("run command failed")
 
 
-
-def wait_invoke_finished_get_out(invoke_id,wait_count,wait_interval):
+def wait_invoke_finished_get_out(invoke_id, wait_count, wait_interval):
     for i in range(wait_count):
-        status,output = get_invoke_result(invoke_id)
-        if status not in ["Running","Pending","Stopping"]:
-            return status,output
+        status, output = get_invoke_result(invoke_id)
+        if status not in ["Running", "Pending", "Stopping"]:
+            return status, output
         time.sleep(wait_interval)
 
-    logger.error("after wait %d times, still can not wait invoke-id %s finished")
-    return "",""
+    logger.error(
+        "after wait %d times, still can not wait invoke-id %s finished")
+    return "", ""
+
 
 def remove_containers():
     """移除所有容器
@@ -92,6 +95,7 @@ def remove_containers():
     invoke_id = run_command(cmdtype, cmdcontent)
     log_cmd_status(invoke_id)
 
+
 def remove_images():
     """移除所有镜像
     """
@@ -105,6 +109,7 @@ def remove_images():
     # 执行命令
     invoke_id = run_command(cmdtype, cmdcontent)
     log_cmd_status(invoke_id)
+
 
 def pull_images():
     """拉取所有镜像
@@ -120,6 +125,7 @@ def pull_images():
     invoke_id = run_command(cmdtype, cmdcontent)
     log_cmd_status(invoke_id, 10, 10)
 
+
 def run_containers():
     """运行所有容器
     """
@@ -128,20 +134,23 @@ def run_containers():
     # 设置云助手命令的命令内容
     cmdcontent = f"""
     #!/bin/bash
-    sudo docker run --name repo-docs -dp 127.0.0.1:5173:80 {vpc_registry}/c2333kfeng/repo-docs
+    sudo docker run --name repo-docs -dp 127.0.0.1:5173:80 -v /home/ecs-user/repo/nginx.conf:/etc/nginx/nginx.conf:ro {vpc_registry}/c2333kfeng/repo-docs
     """
     # 执行命令
     invoke_id = run_command(cmdtype, cmdcontent)
     log_cmd_status(invoke_id)
 
-def log_cmd_status(invoke_id, count = 5, interval = 5):
+
+def log_cmd_status(invoke_id, count=5, interval=5):
     """打印命令运行状态
     """
     logger.info('run command, invoke-id: %s' % invoke_id)
     # 等待命令执行完成,循环查询10次，每次间隔5秒，查询次数和间隔请根据实际情况配置
-    status,output = wait_invoke_finished_get_out(invoke_id, count, interval)
+    status, output = wait_invoke_finished_get_out(invoke_id, count, interval)
     if status:
-        logger.info("invoke-id execute finished,status: %s,output: %s" % (status,output))
+        logger.info("invoke-id execute finished,status: %s,output: %s" %
+                    (status, output))
+
 
 def run_task():
     remove_containers()
